a0f9fc043c8107e2b9e36b80675fdcf6dd9b584d
rcreager@nd.edu <rcreager@nd.edu@ea07653f-3dce-2597-97fd-38a04fa41daa>
2012-04-16 14:15:02 +0000

    Commented_Piece

--- trunk/piece.h
+++ trunk/piece.h
@@ -8,19 +8,19 @@ class piece {
  public:
   piece();
   ~piece();
-  int getIsNull();   //Returns the int value of each variable
-  int getTeam();
-  int getIsKing();
-  char getRep();
+  int getIsNull();      //Returns the int value of each variable
+  int getTeam();        //Returns 1 if x, -1 if o, 0 if blank
+  int getIsKing();      //Returns 1 if king, 0 if not
+  char getRep();        //Returns the char representing the piece
   void setIsNull(int);  //Allows you to change the int value of a variable
-  void setTeam(int);
-  void setIsKing(int);
+  void setTeam(int);    //Sets the team
+  void setIsKing(int);  //Sets a piece to king
 
  private:
   int isNull; //if space is on unplayable diagonal, isNull is true
   int team;   //Keeps track of color, 1 is x, -1 is o, 0 is blank
   int isKing; //starts off false, becomes true if piece goes to back row
-  char rep;  //holds the team's character based on isNull, team, and isKing
+  char rep;   //holds the team's character based on isNull, team, and isKing
 
 };
 
