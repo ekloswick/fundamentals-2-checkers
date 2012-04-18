3063c17fdd800e74880f9f72935679f28d696dce
ccahilla@nd.edu <ccahilla@nd.edu@ea07653f-3dce-2597-97fd-38a04fa41daa>
2012-04-18 14:39:45 +0000

    Working_Alternation

--- trunk/checkers.h
+++ trunk/checkers.h
@@ -14,11 +14,15 @@ class checkers {
   ~checkers();
   void print();        //prints the current state of the board
   void play();         //play checkers!
-  int checkForJump();  //returns 1 if x can move, -1 if o can move, 0 if no moves available for current player
+  void countPieces();   //counts the number of x's and o's
+  int checkForMove();  //returns 1 if x can move, -1 if o can move, 0 if no moves available for current player
+  int checkForJump();  //returns 1 if x can jump, -1 if o can jump, 0 if no jumps available for current player
   int checkForWin();   //returns 1 if x wins, -1 if o wins, 0 if nobody has won
 
  private:
   int turn;            //1 if x's turn, -1 if o's turn
+  int xcount;          //Keeps track of number of x's on board
+  int ocount;          //Keeps track of number of o's on board
   vector <vector <piece> > board; //Fill a 2D board with type piece
 };
 
